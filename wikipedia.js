2031187abd9f55a161befee4b582b9cb8dcbcf79
12343-debug <1kiran1234567990@gmail.com>
2024-12-28 13:45:53 +0530

    Add files via upload

---
+++ wikipedia.js
@@ -0,0 +1,81 @@
+let searchInputEl=document.getElementById("searchInput");
+let searchResultEl=document.getElementById("searchResults");
+let spinnerEl=document.getElementById("spinner");
+
+function createAndAppendSearchResult(result){
+    let {title,link,description} = result;
+    // 1.Div-Container -- result Item
+     let resultItemEl=document.createElement("div");
+     resultItemEl.classList.add("results-item");
+
+     searchResultEl.appendChild(resultItemEl);
+
+    // 2.Anchor Title -- result-title
+    let resultTitleEl=document.createElement("a");
+    resultTitleEl.classList.add("result-title");
+    resultTitleEl.textContent = title;
+    resultTitleEl.href = link;
+    resultTitleEl.target = "_blank";
+
+    resultItemEl.appendChild(resultTitleEl);
+
+     // 3.Title Break
+    let titleBreakEl = document.createElement("br");
+    resultItemEl.appendChild(titleBreakEl);
+
+    // 4.Anchor URL -- result-url
+    let urlEl = document.createElement("a");
+    urlEl.classList.add("result-url");
+    urlEl.href = link;
+    urlEl.target = "_blank";
+    urlEl.textContent = link;
+
+    resultItemEl.appendChild(urlEl);
+
+    // 5.Line Break
+    let lineBreakEl = document.createElement("br");
+    resultItemEl.appendChild(lineBreakEl);
+
+    // 6.paragraph Description -- line-description
+    let descriptionEl = document.createElement("p");
+    descriptionEl.classList.add("line-description");
+    descriptionEl.textContent = description;
+
+    resultItemEl.appendChild(descriptionEl);
+
+}
+
+function displayResults(searchResults){
+    spinnerEl.classList.toggle("d-none");
+    for(let result of searchResults){
+        createAndAppendSearchResult(result);
+
+    }
+   
+}
+
+function searchWivipedia(event){
+
+    if(event.key==="Enter"){
+
+        searchResultEl.textContent = "";
+        spinnerEl.classList.toggle("d-none");
+        let searchInput=searchInputEl.value;
+        let url ="https://apis.ccbp.in/wiki-search?search=" + searchInput;
+        let options={
+            method: "GET"
+        }
+
+        fetch(url,options)
+        .then(function(response){
+            return response.json();
+        })
+
+        .then(function(jsonData){
+            let {search_results}=jsonData;
+            displayResults(search_results);
+        });
+    }
+
+}
+searchInputEl.addEventListener("keydown",searchWivipedia);
